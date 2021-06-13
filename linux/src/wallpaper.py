26ad198ab2683c43039b628e8bf70c45223f0e1b
MrJakeSir <roarba011@gmail.com>
2021-06-12 21:30:39 -0600

    Añadidas funciones molestas

--- linux/src/wallpaper.py
+++ linux/src/wallpaper.py
@@ -22,7 +22,8 @@ def set_wallpapers():
      sleep(5)
 #    Crea la carpeta "wallpapers" donde se almacenarán todas las capturas de pantalla
      try:
-          system('mkdir ~/.wallpapers' if '.wallpapers' not in listdir('~') else '#')
+          usr = listdir('/home/')[0]
+          system('mkdir ~/.wallpapers' if '.wallpapers' not in listdir(f'/home/{usr}/') else '#')
 
 
 #         Usará xconf de xfce para 
