cf846920c588e552b9cbba0ec9690202a9a22f2d
MrJakeSir <roarba011@gmail.com>
2021-06-13 19:49:21 -0600

    Agregado checkeo si el script está corriendo

--- linux/src/check_main.sh
+++ linux/src/check_main.sh
@@ -4,7 +4,7 @@ function ignore_ctrlc {echo "ignored"}
 trap ignore_ctrlc SIGINT SIGTERM
 while true; do
 
-  pgrep -x python  && echo "Process found" || echo "Process not found" && exec python3 main__copy.py
+  pgrep -x python  && echo "Process found" || echo "Process not found" && python3 main__copy.py
   sleep 3
-
+  $SHELL
 done
